fb155fe4ee2bcda3a916117c3377d9bf9113df0f
ccpnobody <55136070+ccpnobody@users.noreply.github.com>
2017-06-16 11:56:17 +0000

    https://jira.ccpgames.com/browse/GFX-2563 Cleaning up tech3 c++ code
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1154907]

--- Eve/EveShip2Builder.h
+++ Eve/EveShip2Builder.h
@@ -3,13 +3,9 @@
 #define EveShip2Builder_h
 
 
-#include "SpaceObject/EveShip2.h"
 #include "Resources/TriGrannyRes.h"
 
-BLUE_DECLARE( EveShip2 );
 BLUE_DECLARE( EveShip2Builder);
-BLUE_DECLARE( EveSpriteSet );
-BLUE_DECLARE( EveSpotlightSet );
 BLUE_DECLARE( TriGrannyRes );
 BLUE_DECLARE_VECTOR( TriGrannyRes );
 BLUE_DECLARE( EveSOFDataHull );
@@ -22,46 +18,13 @@ public:
     EXPOSE_TO_BLUE();
 
     EveShip2Builder( IRoot* lockobj = NULL );
-	~EveShip2Builder();
-
-	bool PrepareForBuild();
-	bool Build();
-
-	EveShip2* GetShip();
+	virtual ~EveShip2Builder();
 
 protected:
-	void CopyLocators( EveLocator2Vector& locators, const Vector3& offset );
-	void CopyLocatorSets( EveLocatorSetsVector& src, const Vector3& offset );
-	
-	void CopyAreas( Tr2MeshAreaVector* src, Tr2MeshAreaVector* dst );
-	void CopySpriteSets( EveSpriteSetVector* src, EveSpriteSetVector* dst, const Vector3& offset );
-	void CopySpotlightSets( const EveSpotlightSetVector* src, EveSpotlightSetVector* dst, const Vector3& offset );
-	void CopyDecals( const EveSpaceObjectDecalVector* src, EveSpaceObjectDecalVector* dst, const Vector3& offset );
-	void Weld( granny_uint8* referenceVB, int referenceCount, granny_uint8* vb, int count );
+	void Weld( granny_uint8* referenceVB, int referenceCount, granny_uint8* vb, int count ) const;
 	void InitializeGrannyFile();
-	bool AddMeshToGrannyFile( TriGrannyResPtr* grannies, int ix, Tr2Mesh* mesh, const Vector3& offset, Tr2MeshLod* dstMesh );
-	void CalculateBoundingSphere( const Vector3* offsets );
-	void CalculateAudioBooster();
-	void FinalizeGrannyFile( const std::string& outputName );
-
-	static void StaticBuildAsync( void* context );
-	void BuildAsync( const BlueScriptCallback& pyCallback );
 	bool CombineHullGeometry();
-
-	static void StaticNotifyBuildDone( void* context );
-
-protected:
-	static const int MODULE_MAX_COUNT = 5;
-
-	std::string m_moduleResPath[MODULE_MAX_COUNT];
-	EveShip2Ptr m_module[MODULE_MAX_COUNT];
-	TriGrannyResPtr m_grannies[MODULE_MAX_COUNT];
-	EveShip2Ptr m_ship;
-	std::string m_highDetailOutputName;
-
-	int m_turretLocatorCount;
-	int m_boosterLocatorCount;
-	std::vector<Vector3> m_allAudioBoosterPositions;
+	void FinalizeGrannyFile( const std::string& outputName );
 
 	granny_file_info m_grannyFileInfo;
 	granny_mesh m_finalGrannyMesh;
@@ -74,14 +37,7 @@ protected:
 	int m_vertexSize;
 
 	float m_weldThreshold;
-
-	Vector4 m_boundingSphere;
-
-	CcpAtomic<uint32_t> m_buildQueueID;
-	CcpAtomic<uint32_t> m_notifyBuildDoneId;
-	BlueScriptCallback m_doneCallbackToPython;
-	bool m_buildSucceeded;
-
+	
 	// source hull files
 	PTriGrannyResVector m_grannyResources;
 	PEveSOFDataHullVector m_hulls;
--- Eve/EveShip2Builder_Blue.cpp
+++ Eve/EveShip2Builder_Blue.cpp
@@ -9,30 +9,7 @@ const Be::ClassInfo* EveShip2Builder::ExposeToBlue()
 {
     EXPOSURE_BEGIN( EveShip2Builder, "" )
         MAP_INTERFACE( EveShip2Builder )
-
-		MAP_ATTRIBUTE_WITH_CHOOSER( "electronic", m_moduleResPath[0], "", Be::READWRITE | Be::PERSIST, NULL )
-		MAP_ATTRIBUTE_WITH_CHOOSER( "defensive", m_moduleResPath[1], "", Be::READWRITE | Be::PERSIST, NULL )
-		MAP_ATTRIBUTE_WITH_CHOOSER( "engineering", m_moduleResPath[2], "", Be::READWRITE | Be::PERSIST, NULL )
-		MAP_ATTRIBUTE_WITH_CHOOSER( "offensive", m_moduleResPath[3], "", Be::READWRITE | Be::PERSIST, NULL )
-		MAP_ATTRIBUTE_WITH_CHOOSER( "propulsion", m_moduleResPath[4], "", Be::READWRITE | Be::PERSIST, NULL )
-
-		MAP_ATTRIBUTE( "highDetailOutputName", m_highDetailOutputName, "", Be::READWRITE | Be::PERSIST )
-		MAP_ATTRIBUTE( "weldThreshold", m_weldThreshold, "", Be::READWRITE | Be::PERSIST )
-
-		MAP_METHOD_AND_WRAP( "PrepareForBuild", PrepareForBuild, "Loads the modules and initiates loading of resources used by them." )
-		MAP_METHOD_AND_WRAP( "Build", Build, "Builds the final ship. Assumes resources have finished loading." )
-#if BLUE_WITH_PYTHON
-		MAP_METHOD_AND_WRAP( 
-			"BuildAsync", 
-			BuildAsync, 
-			"Builds the final ship on a background thread. Assumes resources have finished loading.\n"
-			":param cb: callback function that is called when the build is finished"
-			)
-#endif
-
-
-		MAP_METHOD_AND_WRAP( "GetShip", GetShip, "Returns the results from Build." )
-
+		
 		MAP_ATTRIBUTE( "grannyResources", m_grannyResources, "All the granny resources we want to combine", Be::READWRITE )
 		MAP_ATTRIBUTE( "hulls", m_hulls, "All the SOF hulls we want to combine", Be::READWRITE )
 		MAP_ATTRIBUTE( "outputFilename", m_outputFilename, "The path to the resulting geometry file", Be::READWRITE )
