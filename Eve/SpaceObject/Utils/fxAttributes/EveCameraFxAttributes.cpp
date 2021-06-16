487a0045c8bf20e30d112c7eab25e8af236c693f
Henrý Þór Jónsson <henry@ccpgames.com>
2021-06-16 14:05:45 +0000

    changing EveCameraFxAttribute to calculate based on it's direct childOwner if available instead of using the spaceObject translation
    With this change some fx on the 2xfe monument is worth taking a look at along with the science ball on this defect
    
    https://jira.ccpgames.com/browse/EO-5543
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1911955]

--- Eve/SpaceObject/Utils/fxAttributes/EveCameraFxAttributes.cpp
+++ Eve/SpaceObject/Utils/fxAttributes/EveCameraFxAttributes.cpp
@@ -24,7 +24,15 @@ void EveCameraFxAttributes::UpdateAsyncronous( EveUpdateContext& updateContext, 
 	{
 		params.spaceObjectParent->GetModelCenterWorldPosition( objPos );
 	}
-
+	
+	if( nullptr != params.childParent )
+	{
+		Matrix localToWorldTransform;
+		params.childParent->GetLocalToWorldTransform( localToWorldTransform );
+		m_rotationWithChildTransform = Vector3( localToWorldTransform._13, localToWorldTransform._23, localToWorldTransform._33 );
+		objPos = localToWorldTransform.GetTranslation();
+	}
+	
 	const Vector3 camPos = Tr2Renderer::GetViewPosition();
 	const Vector3 vec2obj = objPos - camPos;
 	const Matrix view = Tr2Renderer::GetViewTransform();
@@ -32,13 +40,7 @@ void EveCameraFxAttributes::UpdateAsyncronous( EveUpdateContext& updateContext, 
 	m_distanceToCamera = Length( vec2obj );
 	m_lookAngleToObject = -( Dot( Tr2Renderer::GetViewLookAt(), vec2obj ) / m_distanceToCamera );
 
-	if( nullptr != params.childParent )
-	{
-		Matrix localToWorldTransform;
-		params.childParent->GetLocalToWorldTransform( localToWorldTransform );
-		
-		m_rotationWithChildTransform = Vector3( localToWorldTransform._13, localToWorldTransform._23, localToWorldTransform._33 );
-	}
+	
 	
 	m_objectRotation = Vector3( params.localToWorldTransform._13, params.localToWorldTransform._23, params.localToWorldTransform._33 );
 	m_cameraRotation = Vector3( view._13, view._23, view._33 );
